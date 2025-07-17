b75c66b323c459a1c04b75c1f441a241cc3fbca8
Rafid Hoda <rafidhoda@gmail.com>
2025-07-17 14:03:51 +0200

    Add smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollBehavior: 'smooth' }}>
       <head>
         <script 
           defer 
